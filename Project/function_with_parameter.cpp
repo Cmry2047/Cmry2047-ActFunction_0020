2b2e0c2e2293619f3f8d1ad915c9edcafad44aff
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-02 12:41:05 +0700

    Menambahkan fungsi luasPersegi dan kelilingPersegi dengan parameter

--- Project/function_with_parameter.cpp
+++ Project/function_with_parameter.cpp
@@ -11,6 +11,16 @@ void input()
     cin >> lebar;  
 }
 
+int luasPersegi(int a, int b)
+{
+    return a * b;
+}
+
+int kelilingPersegi(int a, int b)
+{
+    return 2 * (a + b);
+}
+
 int main()
 {
 
