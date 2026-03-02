fbafcbb4f5abdd4da8b5f95f998a58ef6a6bd952
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-02 12:43:31 +0700

    Membuat struktur awal program untuk Luas Lingkaran dengan parameter

--- Project/Luas_Lingkaran.cpp
+++ Project/Luas_Lingkaran.cpp
@@ -4,24 +4,7 @@ using namespace std;
 float r;
 float phi = 3.14159;
 
-void input()
-{
-    cout << "Masukkan jari-jari: ";
-    cin >> r;
-}
-
-float luas(float a)
-{
-    return phi * a * a;
-}
-
-void output()
-{
-    cout << "Luasnya adalah: " << luas(r);
-}
-
 int main()
 {
-    input();
-    output();
+
 }
